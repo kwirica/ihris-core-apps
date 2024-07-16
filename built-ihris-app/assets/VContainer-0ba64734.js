9850fd37b875524e87ec2e6260df438bf48a88b7
Nobert Mijumbi <nobertmn@gmail.com>
2024-07-16 12:10:38 +0300

    Rebuilt app

---
+++ built-ihris-app/assets/VContainer-0ba64734.js
@@ -0,0 +1,1 @@
+import{$ as n,a0 as o,af as r,a2 as l,aI as i,a4 as c,a as u}from"./index-5c26168b.js";const m=n({fluid:{type:Boolean,default:!1},...o(),...r()},"VContainer"),p=l()({name:"VContainer",props:m(),setup(a,e){let{slots:s}=e;const{rtlClasses:t}=i();return c(()=>u(a.tag,{class:["v-container",{"v-container--fluid":a.fluid},t.value,a.class],style:a.style},s)),{}}});export{p as V};
